d1cd7616dce217fa84c70b9e58e09a5b8f26f138
olga-demina <olga.demina1410@gmail.com>
2022-11-07 13:16:12 +0100

    Esempio componenti AppTitle e AppBanner

---
+++ src/components/AppBanner.vue
@@ -0,0 +1,33 @@
+<script>
+export default {
+    data() {
+        return {
+            imgName: `milano.jpg`
+        }
+    },
+    methods: {
+        getImagePath() {
+            return new URL(`../assets/img/${this.imgName}`, import.meta.url).href;
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="banner">
+        <img :src="getImagePath()" alt="">
+    </div>
+</template>
+
+<style scoped>
+.banner {
+    width: 100%;
+    height: 30vh;
+}
+
+.banner img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+}
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+    data() {
+        return {
+            title: "Mio titolo"
+        }
+    }
+}
+</script>
+
+<template>
+    <h2>{{ title }}</h2>
+</template>
+
+<style scoped>
+
+</style>
